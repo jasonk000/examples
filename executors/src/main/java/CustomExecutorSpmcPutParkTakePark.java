0ee7ba9edfb50550479dcdec4d17ce1db27a4116
jasonk000 <jasonk@bluedevel.com>
2014-09-21 23:29:06 +1000

    Added executors sample project to cover http://fasterjava.blogspot.com.au/2014/09/writing-non-blocking-executor.html

---
+++ executors/src/main/java/CustomExecutorSpmcPutParkTakePark.java
@@ -0,0 +1,237 @@
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.atomic.*;
+import java.util.concurrent.*;
+import java.util.Collection;
+import java.util.List;
+import java.util.concurrent.locks.LockSupport;
+import java.util.Queue;
+
+public class CustomExecutorSpmcPutParkTakePark {
+ 
+    static double calculatePiFor(int slice, int nrOfIterations) {
+        double acc = 0.0;
+        for (int i = slice * nrOfIterations; i <= ((slice + 1) * nrOfIterations - 1); i++) {
+            acc += 4.0 * (1 - (i % 2) * 2) / (2 * i + 1);
+        }
+        return acc;
+    }
+ 
+    private static long piTest(final int numThreads) throws InterruptedException {
+ 
+        final int numMessages = 1000000;
+        final int step = 100;
+
+        final ExecutorService test = new QueueExecutor(numThreads);
+        final AtomicInteger latch = new AtomicInteger(numMessages);
+        final AtomicReference<Double> result = new AtomicReference<>(0.0);
+        final AtomicLong timSum = new AtomicLong(0);
+ 
+        final long tim = System.currentTimeMillis();
+        for ( int i= 0; i< numMessages; i++) {
+            final int finalI = i;
+            test.execute(new Runnable() {
+                public void run() {
+                    double res = calculatePiFor(finalI, step);
+                    Double expect;
+                    boolean success;
+                    do {
+                        expect = result.get();
+                        success = result.compareAndSet(expect,expect+res);
+                    } while( !success );
+                    int lc = latch.decrementAndGet();
+                    if (lc == 0 ) {
+                        long l = System.currentTimeMillis() - tim;
+                        timSum.set(timSum.get()+l);
+                        System.out.println("pi: " + result.get() + " t:" + l + " finI " + finalI);
+                        test.shutdown();
+                    }
+                }
+            });
+        }
+        while (latch.get() > 0 ) {
+            LockSupport.parkNanos(1000*500); // don't care as 0,5 ms are not significant per run
+        }
+        return timSum.get();
+    }
+ 
+    public static void main( String arg[] ) throws Exception {
+        final int MAX_ACT = 8;
+        String results[] = new String[MAX_ACT];
+ 
+        for ( int numActors = 1; numActors <= MAX_ACT; numActors++ ) {
+            long sum = 0;
+            for ( int ii=0; ii < 30; ii++) {
+                long res = piTest(numActors);
+                if ( ii >= 20 ) {
+                    sum+=res;
+                }
+            }
+            results[numActors-1] = "average "+numActors+" threads : "+sum/10;
+        }
+ 
+        for (int i = 0; i < results.length; i++) {
+            String result = results[i];
+            System.out.println(result);
+        }
+          
+    }
+
+    private static class QueueExecutor<T> implements ExecutorService {
+
+        private volatile boolean running;
+        private volatile boolean stopped;
+        private final int threadCount;
+        private final SpmcQueue queue;
+    	private final Thread[] threads;
+
+        public QueueExecutor(int threadCount) {
+            this.threadCount = threadCount;
+	    this.queue = new SpmcQueue(2^10);
+            running = true;
+            stopped = false;
+            threads = new Thread[threadCount];
+            for(int i = 0; i < threadCount; i++) {
+                threads[i] = new Thread(new Worker());
+		threads[i].start();
+            }
+        }
+
+        public void execute(Runnable runnable) {
+            try {
+                queue.put(runnable);
+	    } catch (InterruptedException ie) {
+	        throw new RuntimeException(ie);
+	    }
+	}
+
+        private class SpmcQueue { //implements BlockingQueue<Runnable> {
+            private final Runnable[] buffer;
+            
+            private final int capacity;
+ 
+            // need a counter that producer can lazyset publish to for write
+            AtomicLong nextSlotToWrite = new AtomicLong(1);
+             
+            // need a counter that consumers can use to track where 
+            // it is safe to read from 
+            AtomicLong lastConsumed = new AtomicLong(0);
+
+            public SpmcQueue(int capacity) {
+                this.capacity = capacity;
+                this.buffer = new Runnable[capacity];
+            }
+
+            public void put(Runnable r) throws InterruptedException {
+                while (true) {
+                    long toWrite = nextSlotToWrite.get();
+                    if (toWrite < (lastConsumed.get() + capacity)) {
+                        buffer[(int) (toWrite % capacity)] = r;
+                        nextSlotToWrite.incrementAndGet();
+                        // System.out.println("put() win: slot=" + slot + "; lastConsumed=" + lastConsumed.get());
+                        break;
+                    } else {
+                        // System.out.println("put() fail: slot=" + slot + "; lastConsumed=" + lastConsumed.get());
+                        // no space in queue, sleep
+                        LockSupport.parkNanos(1);
+                    }
+                }
+                // throw new InterruptedException();
+            }
+            public Runnable take() throws InterruptedException {
+                while(!Thread.interrupted()) {
+                    long lastFilled = nextSlotToWrite.get() - 1;
+                    long lastConsumedCache = lastConsumed.get();
+                    if (lastConsumedCache < lastFilled) {
+                        // fetch the value from buffer first, to avoid a wrap race
+                        int bufferslot = (int) ((lastConsumedCache + 1) % capacity);
+                        Runnable r = buffer[bufferslot];
+                        // attempt to cas
+                        boolean casWon = lastConsumed.compareAndSet(
+                            lastConsumedCache, lastConsumedCache + 1); 
+                        // if cas failed, then we are not the owner, so we need to try again
+                        if (casWon) {
+                            // System.out.println("take() pass: got data lastFilled=" + lastFilled + "; lastConsumedCache=" + lastConsumedCache);
+                            return r;
+                        } else {
+                            // System.out.println("take() cas fail");
+                        }
+                    } else {
+                        // System.out.println("take() fail: waiting for data lastFilled=" + lastFilled + "; lastConsumedCache=" + lastConsumedCache);
+                        // cas failed, do not sleep
+                        LockSupport.parkNanos(1);
+                    }
+                }
+                throw new InterruptedException();
+            }
+        }
+
+        private class Worker implements Runnable {
+            public void run() {
+                while(running) {
+                    Runnable runnable = null;
+                    try {
+                        runnable = queue.take();
+                    } catch (InterruptedException ie) {
+                        // was interrupted - just go round the loop again,
+                        // if running = false will cause it to exit
+                    }
+                    try {
+                        if (runnable != null) { 
+			    runnable.run();
+			}
+                    } catch (Exception e) {
+                        System.out.println("failed because of: " + e.toString());
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+        public void shutdown() {
+            running = false;
+            for(int i = 0; i < threadCount; i++) {
+                threads[i].interrupt();
+                threads[i] = null;
+            }
+            stopped = true;
+        }
+        public boolean isShutdown() {
+            return running;
+        }
+        public boolean isTerminated() {
+            return stopped;
+        }
+
+        // ***************************************************
+
+        public boolean awaitTermination(long timeout, TimeUnit unit) {
+            throw new UnsupportedOperationException("oops!");
+        }
+        public <T> List<Future<T>> invokeAll(Collection<? extends Callable<T>> tasks) {
+            throw new UnsupportedOperationException("oops!");
+        }
+        public <T> List<Future<T>> invokeAll(Collection<? extends Callable<T>> tasks, long timeout, TimeUnit unit) {
+            throw new UnsupportedOperationException("oops!");
+        }
+        public <T> T invokeAny(Collection<? extends Callable<T>> tasks) {
+            throw new UnsupportedOperationException("oops!");
+        }
+        public <T> T invokeAny(Collection<? extends Callable<T>> tasks, long timeout, TimeUnit unit) {
+            throw new UnsupportedOperationException("oops!");
+        }
+        public List<Runnable> shutdownNow() {
+            throw new UnsupportedOperationException("oops!");
+        }
+        public <T> Future<T> submit(Callable<T> task) {
+            throw new UnsupportedOperationException("oops!");
+        }
+        public Future<?> submit(Runnable task) {
+            throw new UnsupportedOperationException("oops!");
+        }
+        public <T> Future<T> submit(Runnable task, T result) {
+            throw new UnsupportedOperationException("oops!");
+        }
+        public String toString() {
+            throw new UnsupportedOperationException("oops!");
+        }
+    }
+}
---
+++ executors/src/main/java/CustomExecutorStripedPi.java
@@ -0,0 +1,190 @@
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.atomic.*;
+import java.util.concurrent.*;
+import java.util.Collection;
+import java.util.List;
+import java.util.concurrent.locks.LockSupport;
+import java.util.Queue;
+
+public class CustomExecutorStripedPi {
+ 
+    static double calculatePiFor(int slice, int nrOfIterations) {
+        double acc = 0.0;
+        for (int i = slice * nrOfIterations; i <= ((slice + 1) * nrOfIterations - 1); i++) {
+            acc += 4.0 * (1 - (i % 2) * 2) / (2 * i + 1);
+        }
+        return acc;
+    }
+ 
+    private static long piTest(final int numThreads) throws InterruptedException {
+ 
+        final int numMessages = 1000000;
+        final int step = 100;
+
+        final ExecutorService test = new LtqExecutor(numThreads);
+        final AtomicInteger latch = new AtomicInteger(numMessages);
+        final AtomicReference<Double> result = new AtomicReference<>(0.0);
+        final AtomicLong timSum = new AtomicLong(0);
+ 
+        final long tim = System.currentTimeMillis();
+        for ( int i= 0; i< numMessages; i++) {
+            final int finalI = i;
+            /*while ( ((ThreadPoolExecutor)test).getQueue().size() > 40000 ) {
+                LockSupport.parkNanos(100);
+		} */
+            test.execute(new Runnable() {
+                public void run() {
+                    double res = calculatePiFor(finalI, step);
+                    Double expect;
+                    boolean success;
+                    do {
+                        expect = result.get();
+                        success = result.compareAndSet(expect,expect+res);
+                    } while( !success );
+                    int lc = latch.decrementAndGet();
+                    if (lc == 0 ) {
+                        long l = System.currentTimeMillis() - tim;
+                        timSum.set(timSum.get()+l);
+                        System.out.println("pi: " + result.get() + " t:" + l + " finI " + finalI);
+                        test.shutdown();
+                    }
+                }
+            });
+        }
+        while (latch.get() > 0 ) {
+            LockSupport.parkNanos(1000*500); // don't care as 0,5 ms are not significant per run
+        }
+        return timSum.get();
+    }
+ 
+    public static void main( String arg[] ) throws Exception {
+        final int MAX_ACT = 8;
+        String results[] = new String[MAX_ACT];
+ 
+        for ( int numActors = 1; numActors <= MAX_ACT; numActors++ ) {
+            long sum = 0;
+            for ( int ii=0; ii < 30; ii++) {
+                long res = piTest(numActors);
+                if ( ii >= 20 ) {
+                    sum+=res;
+                }
+            }
+            results[numActors-1] = "average "+numActors+" threads : "+sum/10;
+        }
+ 
+        for (int i = 0; i < results.length; i++) {
+            String result = results[i];
+            System.out.println(result);
+        }
+          
+    }
+
+    private static class LtqExecutor<T> implements ExecutorService {
+
+        private volatile boolean running;
+        private volatile boolean stopped;
+        private final int threadCount;
+        // private final BlockingQueue<Runnable> queue;
+    	private final BlockingQueue<Runnable>[] queues;
+	private final Thread[] threads;
+	private int counter = 0;
+
+        public LtqExecutor(int threadCount) {
+            this.threadCount = threadCount;
+            // this.queue = new SynchronousQueue();
+	    // this.queue = new LinkedTransferQueue();
+	    // this.queue = new ArrayBlockingQueue(1000);
+            running = true;
+            stopped = false;
+            threads = new Thread[threadCount];
+	    queues = new BlockingQueue[threadCount];
+            for(int i = 0; i < threadCount; i++) {
+		queues[i] = new LinkedTransferQueue();
+                threads[i] = new Thread(new Worker(queues[i]));
+		threads[i].start();
+            }
+        }
+
+        public void execute(Runnable runnable) {
+	    counter = (counter + 1) % threadCount;
+            try {
+                queues[counter].put(runnable);
+	    } catch (InterruptedException ie) {
+		throw new RuntimeException(ie);
+	    }
+	}
+
+        private class Worker implements Runnable {
+	    private final BlockingQueue<Runnable> tasks;
+            public Worker(BlockingQueue<Runnable> tasks) {
+		this.tasks = tasks;
+	    }
+            public void run() {
+                while(running) {
+                    Runnable runnable = null;
+                    try {
+                        runnable = tasks.take();
+                    } catch (InterruptedException ie) {
+                        // was interrupted - just go round the loop again,
+                        // if running = false will cause it to exit
+                    }
+                    try {
+                        if (runnable != null) { 
+			    runnable.run();
+			}
+                    } catch (Exception e) {
+                        System.out.println("failed because of: " + e.toString());
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+        public void shutdown() {
+            running = false;
+            for(int i = 0; i < threadCount; i++) {
+                threads[i].interrupt();
+                threads[i] = null;
+            }
+            stopped = true;
+        }
+        public boolean isShutdown() {
+            return running;
+        }
+        public boolean isTerminated() {
+            return stopped;
+        }
+
+        // ***************************************************
+
+        public boolean awaitTermination(long timeout, TimeUnit unit) {
+            throw new UnsupportedOperationException("oops!");
+        }
+        public <T> List<Future<T>> invokeAll(Collection<? extends Callable<T>> tasks) {
+            throw new UnsupportedOperationException("oops!");
+        }
+        public <T> List<Future<T>> invokeAll(Collection<? extends Callable<T>> tasks, long timeout, TimeUnit unit) {
+            throw new UnsupportedOperationException("oops!");
+        }
+        public <T> T invokeAny(Collection<? extends Callable<T>> tasks) {
+            throw new UnsupportedOperationException("oops!");
+        }
+        public <T> T invokeAny(Collection<? extends Callable<T>> tasks, long timeout, TimeUnit unit) {
+            throw new UnsupportedOperationException("oops!");
+        }
+        public List<Runnable> shutdownNow() {
+            throw new UnsupportedOperationException("oops!");
+        }
+        public <T> Future<T> submit(Callable<T> task) {
+            throw new UnsupportedOperationException("oops!");
+        }
+        public Future<?> submit(Runnable task) {
+            throw new UnsupportedOperationException("oops!");
+        }
+        public <T> Future<T> submit(Runnable task, T result) {
+            throw new UnsupportedOperationException("oops!");
+        }
+        public String toString() {
+            throw new UnsupportedOperationException("oops!");
+        }
+    }
+}
